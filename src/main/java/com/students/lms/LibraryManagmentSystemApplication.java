953d3f2aa9b2332efa32906f9fc3ccb3dd869e06
Dongmche <gmche19@freeuni.edu.ge>
2024-05-29 15:43:35 +0400

    Initial commit

--- src/main/java/com/students/lms/LibraryManagmentSystemApplication.java
+++ src/main/java/com/students/lms/LibraryManagmentSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentManagmentSystemApplication {
+public class LibraryManagmentSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudentManagmentSystemApplication.class, args);
+		SpringApplication.run(LibraryManagmentSystemApplication.class, args);
 	}
 
 }
